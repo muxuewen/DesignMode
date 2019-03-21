7ccb9944dc5ca7c54b87f336c887e365e71c3e91
muxuewen <13716318829@163.com>
2019-03-21 16:03:19 +0800

    init git

---
+++ readme.md
@@ -0,0 +1,1 @@
+disgin model
---
+++ simplefactroy.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+// 简单工厂模式
+class vehicle{
+public:
+	virtual void run() = 0;
+};
+
+class car :public vehicle
+{
+public:
+	virtual void run()
+	{
+		cout << "car run!" << endl;
+	}
+};
+
+class truck :public vehicle
+{
+public:
+	virtual void run()
+	{
+		cout << "truck run!" << endl;
+	}
+};
+
+enum type
+{
+	CAR,
+	TRUCK
+};
+
+class function
+{
+public:
+	vehicle* create(type name)
+	{
+		switch (name)
+		{
+		case CAR:
+			return new car();
+			break;
+		case TRUCK:
+			return new truck();
+			break;
+		default:
+			break;
+		}
+	}
+};
+
+int main(void)
+{
+	function func;
+	func.create(CAR)->run();
+	func.create(TRUCK)->run();
+	system("PAUSE");
+	return 0;
+}
